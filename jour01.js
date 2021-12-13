bb52699d7c3f9906b412b3ae5222f653edd3ce18
Charles <charles.dubois.h@gmail.com>
2021-12-13 13:48:05 +0100

    second commit

--- jour01.js
+++ jour01.js
@@ -1,1 +1,13 @@
+// Jour 1
+// consigne 1
 console.log("Hello World !");
+// consigne 2
+var test = "My name is Charles"
+console.log(test)
+    // consigne 3
+var name = "Charles"
+console.log("Nice to meet you " + name)
+    // consigne 4
+var testLength = "I'm very long !"
+console.log(testLength.length)
+    //consigne 4
