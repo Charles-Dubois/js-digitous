9ed3aa44b3868d0e01b920e08c3bbacfeb844964
Charles <charles.dubois.h@gmail.com>
2022-01-04 11:07:05 +0100

    bonus1

--- jour05/Révisions/Bonus1.js
+++ jour05/Révisions/Bonus1.js
@@ -4,9 +4,20 @@
 - La fonction retourne une string avec le même texte en alternant majuscule / minuscule
 - Appelez votre fonction avec l'argument "Javascript is easy" et vérifiez le résultat
 */
+let upper = "",
+    lower = "",
+    sentence = "";
 
 function makeItSpongeBob(strings) {
-    for (let i = 1; i < strings.length; i + 2) {
-
+    for (i = 0; i <= strings.length; i++) {
+        if ((i % 2 === 0) || (i === 0)) {
+            upper = strings.charAt(i).toUpperCase();
+            sentence += upper;
+        } else {
+            lower = strings.charAt(i).toLowerCase();
+            sentence += lower;
+        }
     }
+    console.log(sentence);
 }
+makeItSpongeBob("Javascript");
