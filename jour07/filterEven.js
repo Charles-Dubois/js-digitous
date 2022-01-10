b31396e98e01f2942e800365482da7c00fa559b1
Charles <charles.dubois.h@gmail.com>
2022-01-10 14:39:59 +0100

    ajout exercie filterEven

---
+++ jour07/filterEven.js
@@ -0,0 +1,14 @@
+/*
+## 05 - Filter Even
+
+- Créez une variable `numbers` contenant un tableau qui contiendra les valeurs `1`, `2`, `3`, `4`, `5`, `6`, `7` et `8`
+- Créez une variable `even` qui appellera la méthode `.filter()` pour contenir les nombres **pairs** de `numbers`
+- Affichez les valeurs de `even` dans la console
+*/
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const even = numbers.filter(function(num) {
+    return (num % 2 === 0);
+})
+console.log(even)
